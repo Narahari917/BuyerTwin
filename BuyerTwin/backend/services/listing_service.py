7fdcb653c83d0d7232bf811806a1d0b5423446e9
anvithareddy04 <145006320+anvithareddy04@users.noreply.github.com>
2026-04-19 23:56:32 -0700

    removed extra

--- BuyerTwin/backend/services/listing_service.py
+++
@@ -1,9 +0,0 @@
-from db.connection import get_connection
-
-
-def get_all_listings():
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM listings ORDER BY price;")
-            rows = cur.fetchall()
-            return [dict(row) for row in rows]
